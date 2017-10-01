f9cac2054013cc74f540235c0944f94e4cb2cd65
irql <interruptrequestlevel@gmail.com>
2017-10-01 13:22:05 -0400

    Missed a pipe

--- clone-gitlab.sh
+++ clone-gitlab.sh
@@ -17,6 +17,7 @@ fi
 mkdir $USER
 
 curl -s https://gitlab.com/users/$USER/projects.json \
-    | jq .html | sed -nE 's/^"(.*)"/\1/; s/\\n/\n/g; s/\\//g; p' \
+    | jq .html \
+    | sed -nE 's/^"(.*)"/\1/; s/\\n/\n/g; s/\\//g; p' \
     | sed -nE 's/^<a class="project" href="\/(.+)"><span class="project-full-name">$/\1/p' \
     | xargs -P10 -I% git clone git@gitlab.com:%.git $USER/%
