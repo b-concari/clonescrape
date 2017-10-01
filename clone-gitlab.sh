68f0845bd36dcc29a48a5012ace00c69d234b3e0
irql <interruptrequestlevel@gmail.com>
2017-10-01 13:39:46 -0400

    Git makes the USER folder for us. Also, fixing an issue where the git repos are nested two USER directories deep instead of just one.

--- clone-gitlab.sh
+++ clone-gitlab.sh
@@ -14,10 +14,8 @@ then
     exit 1
 fi
 
-mkdir $USER
-
 curl -s https://gitlab.com/users/$USER/projects.json \
     | jq .html \
     | sed -nE 's/^"(.*)"/\1/; s/\\n/\n/g; s/\\//g; p' \
     | sed -nE 's/^<a class="project" href="\/(.+)"><span class="project-full-name">$/\1/p' \
-    | xargs -P10 -I% git clone git@gitlab.com:%.git $USER/%
+    | xargs -P10 -I% git clone git@gitlab.com:%.git %
